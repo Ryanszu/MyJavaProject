a0aabac2d073005967a64067fb275c7c85dd7003
Ryanszu <ryanszu@hotmail.com>
2018-01-07 12:10:51 +0800

    2018-1-7 First Try

---
+++ src/Set1/HelloWorld.java
@@ -0,0 +1,11 @@
+package Set1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is My First Java Program On GITHUB!");
+
+	}
+
+}

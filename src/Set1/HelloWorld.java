cf70746e17cee8c40cfd3471436e29257c912f84
Ryan <35168478+Ryanszu@users.noreply.github.com>
2018-01-07 13:24:43 +0800

    Update HelloWorld.java

--- src/Set1/HelloWorld.java
+++ src/Set1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is My First Java Program On GITHUB!");
+		System.out.println("This is My First Java Program On GITHUB!!");
 
 	}
 
